3c305473d87872e673e5d1bbc4b89ef79253fa2e
do_xuan_trung_iMac <trung13988@gmail.com>
2021-09-21 12:42:40 +0700

    HEADER - add

---
+++ src/__test__/testHeader/TestHeader.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "../../component/header/Header";
+import Icon from "../../component/icon/Icon";
+import View from "../../component/view/View";
+
+export interface ITestHeaderProps {
+  [key: string]: any;
+}
+
+const TestHeader: React.FC<ITestHeaderProps> = ({ id }) => {
+  return (
+    <View className="my-4">
+      <Header
+        title="Header"
+        color="primary"
+        leftText="Left"
+        onLeftPress={() => {}}
+        onRightPress={() => {}}
+        customRight={
+          (
+            <View className="flex-center-y">
+              <Icon name="filter-list" className="mr-2" onPress={() => {}} />
+              <Icon name="explore" />
+            </View>
+          ) as any
+        }
+      />
+    </View>
+  );
+};
+
+export default TestHeader;
---
+++ src/component/header/Header.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+import ClassNames from "classnames";
+import Text from "../text/Text";
+import View from "../view/View";
+import Button, { IButtonProps } from "../button/Button";
+import TouchableOpacity from "../view/TouchableOpacity";
+import Icon from "../icon/Icon";
+import { getColorValue } from "../../style/modifier";
+import { isDark } from "../../style/color/_color";
+
+export interface IHeaderProps {
+  title?: string;
+  onLeftPress?: (props?: any) => any;
+  onRightPress?: (props?: any) => any;
+  customLeft?: (props?: any) => React.ReactElement | Element;
+  customRight?: (props?: any) => React.ReactElement | Element;
+  leftIcon?: string;
+  leftText?: string;
+  rightIcon?: string;
+  rightText?: string;
+  color?: IButtonProps["color"];
+}
+
+const Header: React.FC<IHeaderProps> = ({
+  title,
+  onLeftPress,
+  onRightPress,
+  customLeft,
+  customRight,
+  leftIcon = "arrow-back",
+  leftText,
+  rightIcon = "more-horiz",
+  rightText,
+  color = "primary",
+}) => {
+  const bgColor = getColorValue(color);
+  const wrapperClass = ClassNames(`flex-center-y px-2 py-3 bg-${color}`);
+  const titleClass = ClassNames(
+    "flex-1 h3 font-weight-bold text-center text-white",
+    {
+      "text-dark": !isDark(bgColor),
+    }
+  );
+
+  const getTextColor = () => {
+    if (isDark(bgColor)) {
+      return "light";
+    }
+    return "dark";
+  };
+
+  const renderLeft = () => {
+    if (customLeft) {
+      if (typeof customLeft === "function") {
+        return customLeft();
+      }
+      return customLeft;
+    }
+    if (leftText) {
+      return (
+        <Button
+          className="px-0"
+          height="auto"
+          variant="trans"
+          color={getTextColor()}
+          onPress={onLeftPress}
+          label={leftText}
+        />
+      );
+    }
+    return (
+      <Icon
+        name={leftIcon}
+        color={getTextColor()}
+        className="px-0"
+        onPress={onLeftPress}
+      />
+    );
+  };
+
+  const renderRight = () => {
+    if (customRight) {
+      if (typeof customRight === "function") {
+        return customRight();
+      }
+      return customRight;
+    }
+    if (rightText) {
+      return (
+        <Button
+          className="px-0"
+          height="auto"
+          variant="trans"
+          color={getTextColor()}
+          onPress={onRightPress}
+          label={rightText}
+        />
+      );
+    }
+    return (
+      <Icon
+        name={rightIcon}
+        color={getTextColor()}
+        className="px-0"
+        onPress={onRightPress}
+      />
+    );
+  };
+
+  return (
+    <View className={wrapperClass}>
+      {onLeftPress && renderLeft()}
+      {title && <Text className={titleClass}>{title}</Text>}
+      {onRightPress && renderRight()}
+    </View>
+  );
+};
+
+export default Header;
