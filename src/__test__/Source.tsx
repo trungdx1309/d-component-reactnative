a85c782014a1bd3bcac1245185334b2e7f37d029
do_xuan_trung_iMac <trung13988@gmail.com>
2021-10-02 15:54:47 +0700

    SOURCE - refactor

--- src/__test__/Source.tsx
+++ src/__test__/Source.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import { StatusBar, StyleSheet, useColorScheme } from "react-native";
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
-import DatePicker from "react-native-datepicker";
-import Button from "../component/button/Button";
-import InputText from "../component/input/InputText";
-import Text from "../component/text/Text";
-import SafeAreaView from "../component/view/SafeAreaView";
-import ScrollView from "../component/view/ScrollView";
-import View from "../component/view/View";
+import { ITabViewProps } from "../component/tab/TabView";
 import TestAvatar from "./testAvatar/TestAvatar";
+import TestButton from "./testButton/TestButton";
+import TestCalendar from "./testCalendar/TestCalendar";
+import TestHeader from "./testHeader/TestHeader";
 import TestImages from "./testImage/TestImages";
 import TestInput from "./testInput/TestInput";
-import TestHeader from "./testHeader/TestHeader";
-import TestCalendar from "./testCalendar/TestCalendar";
-import AwesomeList from "../component/list/awesomeList/AwesomeList";
 import TestSelect from "./testSelect/TestSelect";
-import TestButton from "./testButton/TestButton";
-import { ITabViewProps } from "../component/tab/TabView";
 
 const DATA_SOURCE: ITabViewProps["dataSource"] = [
   { key: "testInput", label: "Input", component: <TestInput /> },
