ecf61d51a8231265d048f3beb4e79ecd799149b1
do_xuan_trung_iMac <trung13988@gmail.com>
2021-10-04 09:57:23 +0700

    INPUT DATE - edit style & add props

--- src/component/input/InputDate.tsx
+++ src/component/input/InputDate.tsx
@@ -1,5 +1,5 @@
 import ClassNames from "classnames";
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useImperativeHandle } from "react";
 import DatePicker, { DatePickerProps } from "react-native-date-picker";
 import _ from "lodash";
 import Colors from "../../style/color/_color";
@@ -31,6 +31,7 @@ export interface IInputDateProps
   onChange?: DatePickerProps["onDateChange"];
   label?: string;
   classNameLabel?: string;
+  classNameText?: string;
   className?: string;
   classNameError?: string;
   cancelText?: string;
@@ -46,34 +47,41 @@ export interface IInputDateProps
   disabled?: boolean;
 }
 
-export interface IInputDateMethod {}
+export interface IInputDateMethod {
+  open: () => any;
+  close: () => any;
+}
 
 const InputDate: React.ForwardRefRenderFunction<
   IInputDateMethod,
   IInputDateProps
-> = ({
-  value,
-  onChange,
-  className,
-  classNameLabel,
-  classNameError,
-  label,
-  height = Sizes.inputHeight,
-  cancelText = "Cancel",
-  confirmText = "Confirm",
-  variant = "standard",
-  format,
-  style,
-  placeholder,
-  error,
-  showIcon = true,
-  iconName = "today",
-  mode,
-  disabled,
-  customInput,
-  customIcon,
-  ...rest
-}) => {
+> = (
+  {
+    value,
+    onChange,
+    className,
+    classNameLabel,
+    classNameText,
+    classNameError,
+    label,
+    height = Sizes.inputHeight,
+    cancelText = "Cancel",
+    confirmText = "Confirm",
+    variant = "standard˝",
+    format,
+    style,
+    placeholder,
+    error,
+    showIcon = true,
+    iconName = "today",
+    mode,
+    disabled,
+    customInput,
+    customIcon,
+    ...rest
+  },
+  ref
+) => {
   const hasBorder =
     variant === "outline" || variant === "pill" || variant === "rounded";
   const wrapperClass = ClassNames(
@@ -85,9 +93,13 @@ const InputDate: React.ForwardRefRenderFunction<
     { "mb-1": hasBorder },
     `${classNameLabel}`
   );
-  const textClass = ClassNames("h4 flex-1", {
-    "text-gray": !value || disabled,
-  });
+  const textClass = ClassNames(
+    "h4 flex-1",
+    {
+      "text-gray": !value || disabled,
+    },
+    classNameText
+  );
   const contentClass = ClassNames("flex-center-y px-2", {
     border: hasBorder,
     "border-bottom-1": variant === "standard",
@@ -127,6 +139,11 @@ const InputDate: React.ForwardRefRenderFunction<
     }
     return res;
   }, [value, onChange]);
+
+  useImperativeHandle(ref, () => ({
+    open: () => setOpenDateModal(true),
+    close: () => setOpenDateModal(false),
+  }));
 
   const renderIcon = () => {
     if (customIcon) {
@@ -188,14 +205,3 @@ const InputDate: React.ForwardRefRenderFunction<
 };
 
 export default React.forwardRef(InputDate);
-
-const styles = {
-  borderOutline: {
-    borderWidth: 1,
-    borderColor: Colors.grey,
-  },
-  borderStandard: {
-    borderColor: Colors.grey,
-    borderBottomWidth: Sizes.borderSmall,
-  },
-};
