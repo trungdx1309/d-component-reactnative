553f16e0ab9293dc597b34ab689e6389bb6399e9
do_xuan_trung_iMac <trung13988@gmail.com>
2021-10-06 14:29:18 +0700

    APP COLORS - edited

--- src/style/constant/AppColors.ts
+++ src/style/constant/AppColors.ts
@@ -5,29 +5,32 @@ const DefaultColors = {
   primaryColor: "#041B47",
   secondary: "rgba(211, 15, 15, 0.87)",
   secondaryColor: "rgba(211, 15, 15, 0.87)",
+  dark: "#0D0F12",
+  light: "#FFFFFF",
+
   textColor: "#221D23",
+
   error: "#E63B2E",
   errorColor: "#E63B2E",
   success: "#ADC76F",
   successColor: "#ADC76F",
   warning: "#FF963C",
   warnColor: "#FF963C",
+  disabled: "#9E9EA1",
+
+  black: "#0D0F12",
+  blackTrans: "rgba(0, 0, 0, 0.8)",
+  white: "#FFFFFF",
+  red: "#E63B2E",
+  green: "#33963D",
+  blue: "#17a2b8",
+  yellow: "#FF963C",
+  muted: "#F5F5F5",
   grey: "#d9d9d9",
   gray: "#d9d9d9",
   greyColor: "#d9d9d9",
   greyLight: "#f2f2f2",
   grayLight: "#f2f2f2",
-  disabled: "#9E9EA1",
-
-  red: "#E63B2E",
-  green: "#33963D",
-  blue: "#17a2b8",
-  yellow: "#FF963C",
-  dark: "#0D0F12",
-  black: "#0D0F12",
-  white: "#FFFFFF",
-  light: "#FFFFFF",
-  muted: "#F5F5F5",
 };
 
 export type ColorKeyType = keyof typeof DefaultColors | "transparent";
