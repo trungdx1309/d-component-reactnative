39dd9f3d0a0d9804eba06a17343a10b8f90b4f1b
do_xuan_trung_iMac <trung13988@gmail.com>
2022-01-07 18:08:27 +0700

    TEXT - add color props

--- src/component/text/Text.tsx
+++ src/component/text/Text.tsx
@@ -7,16 +7,27 @@ import Colors from "../../style/color/_color";
 export interface ITextProps extends TextProps {
   className?: string;
   color?: string;
+  colorDarkMode?: string;
 }
 
 const { dark, light } = Colors;
 
-const Text: React.FC<ITextProps> = ({ children, color, ...rest }) => {
+const Text: React.FC<ITextProps> = ({
+  children,
+  color,
+  colorDarkMode,
+  ...rest
+}) => {
   const { style } = rest;
   const transStyle = getStyleProps(rest);
   const isDarkMode = useColorScheme() === "dark";
+  let textColor = color;
+  if (isDarkMode) {
+    textColor = colorDarkMode || color || light;
+  }
   const defaultStyle = {
     color: color || isDarkMode ? light : undefined,
+    // color: textColor,
   };
   return <RNText style={[defaultStyle, transStyle, style]}>{children}</RNText>;
 };
