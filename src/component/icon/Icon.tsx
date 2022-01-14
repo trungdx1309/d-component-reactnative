1972d98b85b3468ff802f32bc996204dec569e9d
do_xuan_trung_iMac <trung13988@gmail.com>
2022-01-14 15:33:36 +0700

    ICON - add color dark mode props

--- src/component/icon/Icon.tsx
+++ src/component/icon/Icon.tsx
@@ -1,11 +1,15 @@
 import React, { useMemo } from "react";
+import { useColorScheme } from "react-native";
 import { Icon as IconElement, IconProps } from "react-native-elements";
+import { ThemeProps } from "../../interface/iTheme";
 import { ColorKeyType } from "../../style/constant/AppColors";
 import { getColorValue } from "../../style/modifier";
 import Sizes from "../../style/size/_size";
 import { getStyleProps } from "../../style/style";
 
-export interface IIconProps extends Omit<IconProps, "color"> {
+export interface IIconProps
+  extends Omit<IconProps, "color">,
+    Omit<ThemeProps, "useLightColor"> {
   className?: string;
   color?: ColorKeyType;
 }
@@ -15,11 +19,18 @@ const Icon: React.FC<IIconProps> = ({
   type = "material",
   style,
   color,
+  colorDarkMode,
   size = Sizes.iconSize,
   ...rest
 }) => {
   const transStyle = getStyleProps(rest);
-  const colorIcon = useMemo(() => getColorValue(color as any), [color]);
+  const isDarkMode = useColorScheme() === "dark";
+  const colorIcon = useMemo(() => {
+    if (isDarkMode && colorDarkMode) {
+      return getColorValue(colorDarkMode);
+    }
+    return getColorValue(color as any);
+  }, [color]);
 
   return (
     <IconElement
