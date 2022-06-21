39dc8e77d2768227bf18afcd7f5b92f3af4bfe76
do_xuan_trung_iMac <trung13988@gmail.com>
2022-06-21 17:58:45 +0700

    edit test style

--- src/__test__/configurationStyle.ts
+++ src/__test__/configurationStyle.ts
@@ -1,18 +1,24 @@
+/** @format */
+
 import Colors from "../style/color/_color";
 import Sizes from "../style/size/_size";
 import Fonts from "../style/font/_font";
 
-Colors.loadColors({ primary: "red" });
+Colors.loadColors({
+    primary: "red",
+    inputColorFocus: "green",
+    inputColorDark: "yellow",
+});
 //@ts-ignore
 // Sizes.loadSizes({ buttonHeight: 20 });
 Fonts.loadFonts({
-  iosFont: "Prompt-Regular",
-  baseFontSize: 12,
-  label: {
-    fontWeight: "500",
-  },
-  locale: {
-    en: "Poppins-Regular",
-    th: "Prompt-Regular",
-  } as any,
+    iosFont: "Prompt-Regular",
+    baseFontSize: 12,
+    label: {
+        fontWeight: "500",
+    },
+    locale: {
+        en: "Poppins-Regular",
+        th: "Prompt-Regular",
+    } as any,
 });
--- src/__test__/testInput/TestInput.tsx
+++ src/__test__/testInput/TestInput.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { ElementRef, useRef, useState } from "react";
 import DatePicker from "react-native-date-picker";
 import CheckBox from "../../component/checkbox/CheckBox";
@@ -11,151 +13,152 @@ import TouchableOpacity from "../../component/view/TouchableOpacity";
 import View from "../../component/view/View";
 
 export interface ITestInputProps {
-  [key: string]: any;
+    [key: string]: any;
 }
 
 const TestInput: React.FC<ITestInputProps> = ({ id }) => {
-  const dateRef = useRef<ElementRef<typeof DatePicker>>(null);
-  const [openDateModal, setOpenDateModal] = useState(false);
-  const [date, setDate] = useState<any>();
-  const [dateRange, setDateRange] = useState<any[]>([]);
+    const dateRef = useRef<ElementRef<typeof DatePicker>>(null);
+    const [openDateModal, setOpenDateModal] = useState(false);
+    const [date, setDate] = useState<any>();
+    const [dateRange, setDateRange] = useState<any[]>([]);
 
-  return (
-    <ScrollView className="my-4 w-100">
-      {/* <CheckBox /> */}
-      <InputText
-        variant="standard"
-        className="my-2"
-        label="Input Standard"
-        color="red"
-        colorFocus="pink"
-        colorDark="yellow"
-        onPressIcon={() => {}}
-        placeholder="Input Standard"
-        // error="Error Test Input"
-      />
+    return (
+        <ScrollView className="my-4 w-100">
+            {/* <CheckBox /> */}
+            <InputText
+                variant="standard"
+                className="my-2"
+                label="Input Standard"
+                color="red"
+                colorDark="yellow"
+                onPressIcon={() => {}}
+                placeholder="Input Standard"
+                // error="Error Test Input"
+            />
 
-      <InputText
-        variant="rounded"
-        className="my-2"
-        label="Input Rounded"
-        color="red"
-        placeholder="Input Rounded"
-        onPressIcon={() => {}}
-      />
-      <InputSearch
-        variant="pill"
-        className="my-2"
-        onPressIcon={() => {}}
-        label="Search Input"
-        colorFocus="pink"
-        colorDark="yellow"
-        useKeyboardAvoidingView
-        offsetSpaceKeyboard={50}
-      />
-      <InputDate
-        label="Date Input Select Month Year"
-        variant="outline"
-        className="my-2"
-        mode="month"
-        format="MM/YYYY"
-        placeholder="Date Input Select Month Year"
-        onChange={(v) => {
-          setDate(v);
-        }}
-        value={date}
-      />
-      <InputDate
-        label="Date Input Outline"
-        variant="outline"
-        className="my-2"
-        mode="date"
-        placeholder="Date Input Outline"
-        onChange={(v) => {
-          setDate(v);
-        }}
-        disabled
-        value={date}
-      />
-      <InputDate
-        label="Date Input Standard"
-        variant="standard"
-        className="my-2"
-        mode="datetime"
-        onChange={(v) => {
-          setDate(v);
-        }}
-        value={date}
-        // disabled
-        placeholder="Date Input Outline"
-      />
-      <InputDate
-        label="Date Input Rounded"
-        variant="rounded"
-        className="my-2"
-        mode="datetime"
-        placeholder="Date Input Rounded"
-        onChange={(v) => {
-          setDate(v);
-        }}
-        value={date}
-      />
-      <InputDate
-        label="Date Input Pill"
-        variant="pill"
-        className="my-2"
-        mode="date"
-        placeholder="Date Input Pill"
-        onChange={(v) => {
-          setDate(v);
-        }}
-        value={date}
-      />
-      <InputDateRange
-        label="Date Input Range Month Year"
-        className="my-2"
-        value={dateRange}
-        onChange={(v = []) => setDateRange(v)}
-        mode="month"
-        format="MM/YYYY"
-      />
-      <InputDateRange
-        label="Date Input Range"
-        className="my-2"
-        value={dateRange}
-        onChange={(v = []) => setDateRange(v)}
-        mode="date"
-      />
+            <InputText
+                variant="rounded"
+                className="my-2"
+                label="Input Rounded"
+                color="red"
+                placeholder="Input Rounded"
+                onPressIcon={() => {}}
+            />
+            <InputSearch
+                variant="pill"
+                className="my-2"
+                onPressIcon={() => {}}
+                label="Search Input"
+                colorFocus="pink"
+                colorDark="yellow"
+                useKeyboardAvoidingView
+                offsetSpaceKeyboard={50}
+            />
+            <InputDate
+                label="Date Input Select Month Year"
+                variant="outline"
+                className="my-2"
+                mode="month"
+                format="MM/YYYY"
+                placeholder="Date Input Select Month Year"
+                onChange={(v) => {
+                    setDate(v);
+                }}
+                value={date}
+            />
+            <InputDate
+                label="Date Input Outline"
+                variant="outline"
+                className="my-2"
+                mode="date"
+                placeholder="Date Input Outline"
+                onChange={(v) => {
+                    setDate(v);
+                }}
+                disabled
+                value={date}
+            />
+            <InputDate
+                label="Date Input Standard"
+                variant="standard"
+                className="my-2"
+                mode="datetime"
+                onChange={(v) => {
+                    setDate(v);
+                }}
+                value={date}
+                // disabled
+                placeholder="Date Input Outline"
+            />
+            <InputDate
+                label="Date Input Rounded"
+                variant="rounded"
+                className="my-2"
+                mode="datetime"
+                placeholder="Date Input Rounded"
+                onChange={(v) => {
+                    setDate(v);
+                }}
+                value={date}
+            />
+            <InputDate
+                label="Date Input Pill"
+                variant="pill"
+                className="my-2"
+                mode="date"
+                placeholder="Date Input Pill"
+                onChange={(v) => {
+                    setDate(v);
+                }}
+                value={date}
+            />
+            <InputDateRange
+                label="Date Input Range Month Year"
+                className="my-2"
+                value={dateRange}
+                onChange={(v = []) => setDateRange(v)}
+                mode="month"
+                format="MM/YYYY"
+            />
+            <InputDateRange
+                label="Date Input Range"
+                className="my-2"
+                value={dateRange}
+                onChange={(v = []) => setDateRange(v)}
+                mode="date"
+            />
 
-      <InputDate
-        variant="icon"
-        label="Date Input Icon"
-        className="my-2 align-self-start"
-        mode="datetime"
-        placeholder="Date Input Pill"
-        onChange={(v) => {
-          setDate(v);
-        }}
-        value={date}
-      />
-      <InputText
-        variant="outline"
-        className="my-2"
-        label="Input Outline"
-        placeholder="Input Outline"
-        onPressIcon={() => {}}
-        error=""
-        useKeyboardAvoidingView
-      />
-      <InputText
-        variant="pill"
-        className="my-2"
-        label="Input Pill"
-        placeholder="Input Pill"
-        onPressIcon={() => {}}
-        useKeyboardAvoidingView
-      />
-      {/* <DatePicker
+            <InputDate
+                variant="icon"
+                label="Date Input Icon"
+                className="my-2 align-self-start"
+                mode="datetime"
+                placeholder="Date Input Pill"
+                onChange={(v) => {
+                    setDate(v);
+                }}
+                value={date}
+            />
+            <InputText
+                variant="outline"
+                className="my-2"
+                label="Input Outline"
+                placeholder="Input Outline"
+                onPressIcon={() => {}}
+                error=""
+                useKeyboardAvoidingView
+            />
+            <InputText
+                variant="pill"
+                iconName="visibility"
+                className="my-2"
+                label="Input Pill"
+                placeholder="Input Pill"
+                onPressIcon={() => {}}
+                useKeyboardAvoidingView
+                // error="123"
+            />
+            {/* <DatePicker
         modal
         date={new Date()}
         onConfirm={() => setOpenDateModal(false)}
@@ -166,8 +169,8 @@ const TestInput: React.FC<ITestInputProps> = ({ id }) => {
         mode="datetime"
         androidVariant="nativeAndroid"
       /> */}
-    </ScrollView>
-  );
+        </ScrollView>
+    );
 };
 
 export default TestInput;
