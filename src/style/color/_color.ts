8ffe89f51d72bb24e551af1d4ab57cac87ecf0ed
do_xuan_trung_iMac <trung13988@gmail.com>
2022-06-30 15:15:43 +0700

    refactor

--- src/style/color/_color.ts
+++ src/style/color/_color.ts
@@ -1,30 +1,35 @@
-/* eslint-disable import/prefer-default-export */
+/**
+ * /* eslint-disable import/prefer-default-export
+ *
+ * @format
+ */
+
 import _ from "lodash";
 import tinycolor from "tinycolor2";
 import DefaultColors from "../constant/AppColors";
 
 export class ColorsClass {
-  [key: string]: any;
+    [key: string]: any;
 
-  constructor() {
-    Object.assign(this, DefaultColors);
-  }
+    constructor() {
+        Object.assign(this, DefaultColors);
+    }
 
-  /**
-   * Load custom set of colors
-   * arguments:
-   * colors - map of keys and colors values e.g {grey10: '#20303C', grey20: '#43515C'}
-   */
-  loadColors(colors: { [key: string]: string }) {
-    _.forEach(colors, (value, key) => {
-      this[key] = value;
-    });
-  }
+    /**
+     * Load custom set of colors
+     * arguments:
+     * colors - map of keys and colors values e.g {grey10: '#20303C', grey20: '#43515C'}
+     */
+    loadColors(colors: { [key: string]: string }) {
+        _.forEach(colors, (value, key) => {
+            this[key] = value;
+        });
+    }
 }
 
 export const isDark = (color: string) => {
-  const lum = tinycolor(color).getLuminance();
-  return lum < 0.55;
+    const lum = tinycolor(color).getLuminance();
+    return lum < 0.55;
 };
 //@ts-ignore
 const Colors = new ColorsClass();
--- src/style/text/_text.ts
+++ src/style/text/_text.ts
@@ -1,4 +1,9 @@
-/* eslint-disable no-plusplus */
+/**
+ * /* eslint-disable no-plusplus
+ *
+ * @format
+ */
+
 import { StyleSheet } from "react-native";
 import Colors from "../color/_color";
 import Fonts from "../font/_font";
@@ -12,86 +17,86 @@ export const FONT_SIZE_VARIANT = { "font-size": "fontSize" };
 
 const textSizeClass: any = {};
 for (let i = 0; i < 100; i++) {
-  textSizeClass[`font-size-${i}`] = {
-    fontSize: i,
-  };
+    textSizeClass[`font-size-${i}`] = {
+        fontSize: i,
+    };
 }
 
 const textLineHeightClass: any = {};
 for (let i = 0; i <= 50; i += 1) {
-  textLineHeightClass[`line-height-${i}`] = {
-    lineHeight: i,
-  };
+    textLineHeightClass[`line-height-${i}`] = {
+        lineHeight: i,
+    };
 }
 
 export const FONT_WEIGHT_VALUE = {
-  100: "100",
-  200: "200",
-  300: "300",
-  400: "400",
-  500: "500",
-  600: "600",
-  700: "700",
-  800: "800",
-  900: "900",
-  normal: "normal",
-  bold: "bold",
+    100: "100",
+    200: "200",
+    300: "300",
+    400: "400",
+    500: "500",
+    600: "600",
+    700: "700",
+    800: "800",
+    900: "900",
+    normal: "normal",
+    bold: "bold",
 };
 
 export const TEXT_ALIGN_VALUE = {
-  auto: "auto",
-  start: "left",
-  left: "left",
-  end: "right",
-  right: "right",
-  center: "center",
-  justify: "justify",
+    auto: "auto",
+    start: "left",
+    left: "left",
+    end: "right",
+    right: "right",
+    center: "center",
+    justify: "justify",
 };
 
 export const TEXT_DECOR_VALUE = {
-  none: "none",
-  underline: "underline",
-  "line-through": "line-through",
-  "underline line-through": "underline line-through",
+    none: "none",
+    underline: "underline",
+    "line-through": "line-through",
+    "underline line-through": "underline line-through",
 };
 
 export const TEXT_TRANSFORM_VALUE = {
-  capitalize: "capitalize",
-  uppercase: "uppercase",
-  lowercase: "lowercase",
+    capitalize: "capitalize",
+    uppercase: "uppercase",
+    lowercase: "lowercase",
 };
 
 const textColorClass = generateStyleValue({ text: "color" }, Colors);
 
 const textAlignClass = generateStyleValue(
-  TEXT_ALIGN_VARIANTS,
-  TEXT_ALIGN_VALUE
+    TEXT_ALIGN_VARIANTS,
+    TEXT_ALIGN_VALUE
 );
 
 const textDecorClass = generateStyleValue(
-  TEXT_DECOR_VARIANTS,
-  TEXT_DECOR_VALUE
+    TEXT_DECOR_VARIANTS,
+    TEXT_DECOR_VALUE
 );
 
 const textTransformClass = generateStyleValue(
-  TEXT_TRANSFORM_VARIANTS,
-  TEXT_TRANSFORM_VALUE
+    TEXT_TRANSFORM_VARIANTS,
+    TEXT_TRANSFORM_VALUE
 );
 
 const fontWeightClass = generateStyleValue(
-  FONT_WEIGHT_VARIANT,
-  FONT_WEIGHT_VALUE
+    FONT_WEIGHT_VARIANT,
+    FONT_WEIGHT_VALUE
 );
 
 const textStyle = StyleSheet.create({
-  ...textColorClass,
-  ...textAlignClass,
-  ...textDecorClass,
-  ...textTransformClass,
-  ...textSizeClass,
-  ...fontWeightClass,
-  ...textLineHeightClass,
-  ...Fonts.fontClass,
+    ...textColorClass,
+    ...textAlignClass,
+    ...textDecorClass,
+    ...textTransformClass,
+    ...textSizeClass,
+    ...fontWeightClass,
+    ...textLineHeightClass,
+    ...Fonts.fontClass,
 });
 
 export default textStyle;
