c26ede60c61957c0fedd2db9d30566c7c9aab612
do_xuan_trung_iMac <trung13988@gmail.com>
2022-07-18 14:14:22 +0700

    INPUT DATE RANGE - edit props

--- src/component/input/InputDateRange.tsx
+++ src/component/input/InputDateRange.tsx
@@ -1,9 +1,9 @@
 import ClassNames from "classnames";
 import React, {
-  ElementRef,
-  forwardRef,
-  useImperativeHandle,
-  useRef,
+    ElementRef,
+    forwardRef,
+    useImperativeHandle,
+    useRef,
 } from "react";
 import { useColorScheme, ViewStyle } from "react-native";
 import { ColorKeyType } from "../../style/constant/AppColors";
@@ -13,154 +13,161 @@ import Icon from "../icon/Icon";
 import Text from "../text/Text";
 import View from "../view/View";
 import InputDate, {
-  getDateModalTypeFromMode,
-  ICustomInputProps,
-  IInputDateProps,
+    getDateModalTypeFromMode,
+    ICustomInputProps,
+    IInputDateProps,
 } from "./InputDate";
 import { InputErrorView } from "./InputText";
 
 export interface IRangeDateCustomInputProps extends ICustomInputProps {
-  side?: "start" | "end";
+    side?: "start" | "end";
 }
 
 export interface IInputDateRangeProps
-  extends Omit<IInputDateProps, "value" | "onChange"> {
-  value?: any[];
-  onChange?: (props: IInputDateRangeProps["value"]) => any;
-  startText?: string;
-  endText?: string;
-  classNameContent?: string;
-  styleContent?: ViewStyle;
-  colorDarkMode?: ColorKeyType;
-  colorDarkModeContent?: ColorKeyType;
-  customInput?: ((props: IRangeDateCustomInputProps) => Element) | Element;
+    extends Omit<IInputDateProps, "value" | "onChange"> {
+    value?: any[];
+    onChange?: (props: IInputDateRangeProps["value"]) => any;
+    startText?: string;
+    endText?: string;
+    classNameContent?: string;
+    styleContent?: ViewStyle;
+    colorDarkMode?: ColorKeyType;
+    colorDarkModeContent?: ColorKeyType;
+    customInput?: ((props: IRangeDateCustomInputProps) => Element) | Element;
 }
 
 export interface IInputDateRangeMethod {}
 
 const InputDateRange: React.ForwardRefRenderFunction<
-  IInputDateRangeMethod,
-  IInputDateRangeProps
+    IInputDateRangeMethod,
+    IInputDateRangeProps
 > = (
-  {
-    className,
-    classNameLabel,
-    classNameError,
-    classNameContent,
-    error,
-    variant = "outline",
-    label,
-    value,
-    onChange,
-    startText = "Start",
-    endText = "End",
-    style,
-    styleContent,
-    colorDarkMode = "transparent",
-    colorDarkModeContent = "transparent",
-    customInput,
-    mode,
-    ...rest
-  },
-  ref
-) => {
-  const colorScheme = useColorScheme();
-  const hasBorder =
-    variant === "outline" || variant === "pill" || variant === "rounded";
-  const labelClass = ClassNames(
-    `h4`,
-    { "mb-1": hasBorder },
-    `${classNameLabel}`
-  );
-
-  const errorClass = ClassNames(
-    "mt-1",
     {
-      "px-2": variant === "pill",
+        className,
+        classNameLabel,
+        classNameError,
+        classNameContent,
+        error,
+        variant = "outline",
+        label,
+        value,
+        onChange,
+        startText = "Start",
+        endText = "End",
+        style,
+        styleContent,
+        colorDarkMode = "transparent",
+        colorDarkModeContent = "transparent",
+        customInput,
+        mode,
+        ...rest
     },
-    classNameError
-  );
+    ref
+) => {
+    const colorScheme = useColorScheme();
+    const hasBorder =
+        variant === "outline" || variant === "pill" || variant === "rounded";
+    const labelClass = ClassNames(
+        `h4`,
+        { "mb-1": hasBorder },
+        `${classNameLabel}`
+    );
 
-  const endRef = useRef<ElementRef<typeof InputDate>>(null);
+    const errorClass = ClassNames(
+        "mt-1",
+        {
+            "px-2": variant === "pill",
+        },
+        classNameError
+    );
 
-  useImperativeHandle(ref, () => ({}));
+    const endRef = useRef<ElementRef<typeof InputDate>>(null);
 
-  const handleChangeStartTime = (start: any) => {
-    const clone: any = [...(value || [])];
-    clone[0] = start;
-    if (
-      clone[1] &&
-      TimeUtils.convertDateTimeToMili(start) >
-        TimeUtils.convertDateTimeToMili(clone[1])
-    ) {
-      clone[1] = undefined;
-    }
-    onChange && onChange(clone);
-    endRef.current && endRef.current.open(getDateModalTypeFromMode(mode));
-  };
+    useImperativeHandle(ref, () => ({}));
 
-  const handleChangeEndTime = (end: any) => {
-    const clone: any = [...(value || [])];
-    clone[1] = end;
-    onChange && onChange(clone);
-  };
+    const handleChangeStartTime = (start: any) => {
+        const clone: any = [...(value || [])];
+        clone[0] = start;
+        if (
+            clone[1] &&
+            TimeUtils.convertDateTimeToMili(start) >
+                TimeUtils.convertDateTimeToMili(clone[1])
+        ) {
+            clone[1] = undefined;
+        }
+        onChange && onChange(clone);
+        endRef.current && endRef.current.open(getDateModalTypeFromMode(mode));
+    };
 
-  return (
-    <View className={className} style={style} colorDarkMode={colorDarkMode}>
-      {label && <Text className={labelClass}>{label}</Text>}
-      <View
-        className={`flex-center-y ${classNameContent}`}
-        style={styleContent}
-        colorDarkMode={colorDarkModeContent}
-      >
-        <InputDate
-          variant={variant}
-          className="flex-1"
-          classNameText="text-center"
-          showIcon={false}
-          onChange={(v) => handleChangeStartTime(v)}
-          maximumDate={value?.[1]}
-          value={value?.[0]}
-          placeholder={startText}
-          customInput={
-            typeof customInput === "function"
-              ? (props) => {
-                  return customInput({ ...props, side: "start" });
-                }
-              : customInput
-          }
-          mode={mode}
-          {...rest}
-        />
-        <Icon
-          name="arrow-forward"
-          className="mx-2"
-          color={value?.length === 0 ? "gray" : getThemeColor({ colorScheme })}
-        />
-        <InputDate
-          variant={variant}
-          className="flex-1"
-          classNameText="text-center"
-          showIcon={false}
-          onChange={(v) => handleChangeEndTime(v)}
-          minimumDate={value?.[0]}
-          value={value?.[1]}
-          placeholder={endText}
-          ref={endRef}
-          customInput={
-            typeof customInput === "function"
-              ? (props) => {
-                  return customInput({ ...props, side: "end" });
-                }
-              : customInput
-          }
-          mode={mode}
-          {...rest}
-        />
-      </View>
-      {error && <InputErrorView error={error} className={errorClass} />}
-    </View>
-  );
+    const handleChangeEndTime = (end: any) => {
+        const clone: any = [...(value || [])];
+        clone[1] = end;
+        onChange && onChange(clone);
+    };
+
+    return (
+        <View className={className} style={style} colorDarkMode={colorDarkMode}>
+            {label && <Text className={labelClass}>{label}</Text>}
+            <View
+                className={`flex-center-y ${classNameContent}`}
+                style={styleContent}
+                colorDarkMode={colorDarkModeContent}
+            >
+                <InputDate
+                    variant={variant}
+                    className="flex-1"
+                    classNameText="text-center"
+                    placeholder={startText}
+                    mode={mode}
+                    {...rest}
+                    customInput={
+                        typeof customInput === "function"
+                            ? (props) => {
+                                  return customInput({
+                                      ...props,
+                                      side: "start",
+                                  });
+                              }
+                            : customInput
+                    }
+                    showIcon={false}
+                    onChange={(v) => handleChangeStartTime(v)}
+                    maximumDate={value?.[1]}
+                    value={value?.[0]}
+                />
+                <Icon
+                    name="arrow-forward"
+                    className="mx-2"
+                    color={
+                        value?.length === 0
+                            ? "gray"
+                            : getThemeColor({ colorScheme })
+                    }
+                />
+                <InputDate
+                    variant={variant}
+                    className="flex-1"
+                    classNameText="text-center"
+                    placeholder={endText}
+                    ref={endRef}
+                    {...rest}
+                    customInput={
+                        typeof customInput === "function"
+                            ? (props) => {
+                                  return customInput({ ...props, side: "end" });
+                              }
+                            : customInput
+                    }
+                    showIcon={false}
+                    onChange={(v) => handleChangeEndTime(v)}
+                    value={value?.[1]}
+                    mode={mode}
+                    minimumDate={value?.[0]}
+                />
+            </View>
+            {error && <InputErrorView error={error} className={errorClass} />}
+        </View>
+    );
 };
 
 export default forwardRef(InputDateRange);
